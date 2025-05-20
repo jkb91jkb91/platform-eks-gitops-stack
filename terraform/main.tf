6603b26f699c69d5f901c54bcb73b648e90dc656
Jakub <jakub.g26101991@gmail.com>
2025-05-20 13:14:54 +0200

    second commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,6 @@
 #  -target=module.vpcEndpoints
 
 
-
-
 module "vpc" {
   source = "./modules/vpc"
 }
@@ -25,48 +23,22 @@ module "sg" {
   vpc_id = module.vpc.vpc_id
 }
 
-# LB MODULE
-module "lb" {
-  source     = "./modules/lb"
-  vpc_id     = module.vpc.vpc_id
-  subnet1_id = module.vpc.public1_subnet_id
-  subnet2_id = module.vpc.public2_subnet_id
-}
-
-# ECS
-module "ecs" {
-  source               = "./modules/ecs"
-  execution_role_arn   = module.iam.execution_role_arn
-  task_role_arn        = module.iam.task_role_arn
-  ecr_repo_url         = module.ecr.ecr_repo_url
-  subnet1_id           = module.vpc.subnet1_id
-  subnet2_id           = module.vpc.subnet2_id
-  sg1_id               = module.sg.sg1_id
-  alb_target_group_arn = module.lb.target_group_arn
-  depends_on           = [module.sg, module.vpc, module.lb]
-}
+# EKS
+# module "eks" {
+#   source = "./modules/eks"
+#   vpc_id = module.vpc.vpc_id
+# }
 
 
-# VPC-ENDPOINT
-module "vpcEndpoints" {
-  source             = "./modules/vpcEndpoints"
-  vpc_id             = module.vpc.vpc_id
-  endpoint_sg_id     = module.sg.endpoint_sg_id
-  task_sg_id         = module.sg.task_sg_id
-  priv_subnet1_id    = module.vpc.private1_subnet_id
-  priv_subnet2_id    = module.vpc.private2_subnet_id
-  s3_route_table_ids = module.vpc.s3_route_table_ids
-  depends_on         = [module.sg, module.vpc]
-}
 
-# ECR
-module "ecr" {
-  source                 = "./modules/ecr"
-  fargate_extra_role_arn = module.iam.fargate_extra_role_arn
-}
+# # ECR
+# module "ecr" {
+#   source                 = "./modules/ecr"
+#   fargate_extra_role_arn = module.iam.fargate_extra_role_arn
+# }
 
 # EC2
-# module "ec2" {
+# module "bastion_host" {
 #   source        = "./modules/ec2"
 #   vpc_id        = module.vpc.vpc_id
 #   subnet_id     = module.vpc.public_ec2_subnet_id
@@ -78,16 +50,3 @@ module "ecr" {
 
 
 
-
-
-
-
-
-
-
-# ROUTE53
-# module "route53" {
-#   source        = "./modules/route53"
-#   dns           = module.lb.alb_dns_name
-#   zone_id       =  module.lb.alb_zone_id
-# }
